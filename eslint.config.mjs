96d670e5bdddf4e2e750f00ee5728817dd767f0c
Jan Vollmer <jan@vllmr.dev>
2024-10-13 18:20:59 +0200

    chore(deps): update eslint to v9

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import { fixupPluginRules } from "@eslint/compat";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      "react-hooks": fixupPluginRules(eslintPluginReactHooks),
+    },
+  }
+);
